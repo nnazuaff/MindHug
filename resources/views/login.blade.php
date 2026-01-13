07aaffa513c103cd46859d257c406f23ef5544a9
Fauzan <fauzanzha09@gmail.com>
2026-01-13 11:23:47 +0700

    Penambahan halaman login

---
+++ resources/views/login.blade.php
@@ -0,0 +1,5 @@
+<x-layout :title="$title">
+    <main class="bg-white flex items-center justify-center pt-0">
+        <div class="mx-auto max-w7xl px-4 py-6 sm:px-6 lg:px-8"></div>
+            
+</x-layout>
